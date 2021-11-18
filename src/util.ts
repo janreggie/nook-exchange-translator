a464bcdf5ca0931b4f21fa97e79c6d908d44a166
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2021-11-18 21:14:32 +0800

    corrected CapitalizeName

--- src/util.ts
+++ src/util.ts
@@ -1,14 +1,14 @@
 // capitalize the first letter of a name
 export function CapitalizeName (name : string) : string {
   if (!name) { return '' }
-  if (name >= 'a' && name <= 'z') {
-    const first = name.charAt(0)
+  const first = name.charAt(0)
+  if (first >= 'a' && first <= 'z') {
     return first.toUpperCase() + name.slice(1)
   }
   return name
 }
 
-export function LowercaseName(name : string) : string {
+export function LowercaseName (name : string) : string {
   return name.toLocaleLowerCase()
 }
 
